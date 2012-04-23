18d6cca24ec41511b309124e104621a80911e7a8
Byron Ruth <b@devel.io>
2012-04-23 17:17:28 -0400

    Add fabfile for easy deployment

---
+++ fabfile.py
@@ -0,0 +1,9 @@
+from fabric import *
+
+env.hosts = ['thedevel.webfactional.com']
+
+def deploy():
+    with cd('~/sites/devel.io'):
+        run('git pull')
+        with prefix('rvm use 1.9.3'):
+            run('jekyll')
