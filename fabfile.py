04faebfcae4645fcfd2a0a57ac5389c0d23f362a
Byron Ruth <b@devel.io>
2012-04-30 13:27:56 -0400

    Remove hard-coded Ruby version

--- fabfile.py
+++ fabfile.py
@@ -5,5 +5,4 @@ env.hosts = ['thedevel.webfactional.com']
 def deploy():
     with cd('~/sites/devel.io'):
         run('git pull')
-        with prefix('rvm use 1.9.3'):
-            run('jekyll')
+        run('jekyll')
