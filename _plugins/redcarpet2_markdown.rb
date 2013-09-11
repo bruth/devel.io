eaf3f26caa3cff231210f4c1764960c06a01e6d7
Byron Ruth <b@devel.io>
2013-09-11 08:52:29 -0400

    Remove deprecated attributes on custom Markdown plugin

--- _plugins/redcarpet2_markdown.rb
+++ _plugins/redcarpet2_markdown.rb
@@ -33,9 +33,6 @@ class Redcarpet2Markdown < Redcarpet::Render::HTML
 end
 
 class Jekyll::MarkdownConverter
-  safe true
-  priority :low
-
   def extensions
     Hash[ *@config['redcarpet']['extensions'].map {|e| [e.to_sym, true] }.flatten ]
   end
